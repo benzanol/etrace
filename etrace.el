f194258c0b2cb9e4464ffda57120a485f5d7e209
Adam Tillou <adam.tillou@gmail.com>
2026-06-20 11:23:53 -0500

    Track garbage collection pauses

--- etrace.el
+++ etrace.el
@@ -49,6 +49,28 @@ original definition, use \\[elp-restore-function] or \\[elp-restore-all]."
 
 (advice-add #'elp--make-wrapper :around #'etrace--make-wrapper-advice)
 
+(defcustom etrace-track-garbage-collection t
+  "Track garbage collection pauses as function calls in the trace."
+  :type 'boolean)
+
+(defvar etrace--gc-elapsed gc-elapsed
+  "Value of gc-elapsed after the last GC pause.
+
+Used to compute the duration of the next one.")
+
+(defun etrace--gc-hook ()
+  (when (and etrace--trace etrace-track-garbage-collection)
+    (let* ((duration-us (truncate (* 1e6 (- gc-elapsed etrace--gc-elapsed))))
+           (t-after (current-time))
+           (t-before (time-subtract t-after (seconds-to-time (- gc-elapsed etrace--gc-elapsed)))))
+      ;; (message "GC Pause: %sms" (* 0.001 duration-us))
+      (when (>= duration-us etrace-min-duration-us)
+        (push (list ?B 'garbage-collect t-before nil) etrace--trace)
+        (push (list ?E 'garbage-collect t-after nil) etrace--trace))))
+  (setq etrace--gc-elapsed gc-elapsed))
+
+(add-hook 'post-gc-hook #'etrace--gc-hook)
+
 (defun etrace-clear ()
   "Clear the etrace buffer"
   (interactive)
@@ -78,14 +100,10 @@ original definition, use \\[elp-restore-function] or \\[elp-restore-all]."
             "{\"name\":\"%s\",\"cat\":\"\",\"ph\":\"%c\",\"ts\":%d,\"pid\":0,\"tid\":0,\"args\":%s}\n"
             (nth 1 ev) (nth 0 ev)
             (truncate (* 1e6 (- (float-time (nth 2 ev)) start-time)))
-            (let ((ev-args (nth 3 ev)))
-              (if ev-args
-                  (json-encode
-                   (cl-loop for i from 0
-                            for arg in ev-args
-                            collect (cons (format "arg%d" i)
-                                          (format "%S" arg))))
-                "{}")))))
+            (cl-loop for i from 0
+                     for arg in (nth 3 ev)
+                     collect (cons (format "arg%d" i) (format "%S" arg)) into strs
+                     finally return (if strs (json-encode strs) "{}")))))
         (insert "]")
         (save-buffer))))
   (message "Wrote trace to etrace-output-file (%s)!" etrace-output-file)
